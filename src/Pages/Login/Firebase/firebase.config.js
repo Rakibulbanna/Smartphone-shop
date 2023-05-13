6f0621aad0ef26e51cb3a156a2afdf4c98ea8cb7
Rakib ul banna <rakibulbanna@gmail.com>
2023-05-13 22:52:36 +0600

    all set

--- src/Pages/Login/Firebase/firebase.config.js
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
 
-    apiKey: "AIzaSyCvnCoGGIR7KNdOBVDFpcQrGDWLWg9OT44",
-    authDomain: "assignment-12-9ea71.firebaseapp.com",
-    projectId: "assignment-12-9ea71",
-    storageBucket: "assignment-12-9ea71.appspot.com",
-    messagingSenderId: "190964832719",
-    appId: "1:190964832719:web:5c9b8e92274df3666c1aba"
+    apiKey: "AIzaSyCS_tMTsb4FuW54TuMakgUU26lwR_wP5_U",
+    authDomain: "smartphone-shop-28293.firebaseapp.com",
+    projectId: "smartphone-shop-28293",
+    storageBucket: "smartphone-shop-28293.appspot.com",
+    messagingSenderId: "643224410373",
+    appId: "1:643224410373:web:aee03a2ee9b74ec3a4bf6c"
 
 
 
